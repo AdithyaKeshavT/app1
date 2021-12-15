de20cd7d9b650b5072abd37f4e7289813ca9a6e6
AdithyaKeshavT <adityakeshav.ak@gmail.com>
2021-12-15 18:54:28 +0530

    commit

---
+++ src/components/flow.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+	BrowserRouter,
+	Routes,
+	Route,
+	Link
+} from 'react-router-dom';
+
+function flow() {
+    return(
+        <Routes>
+            <Route  />
+        </Routes>
+    );
+}
